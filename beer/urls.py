bf220ccd0865083b7d8f80651d05cf2ad749f938
Eloisa-R <rios.rico.eloisa@gmail.com>
2017-07-17 14:45:25 +0200

    menu design

--- beer/urls.py
+++ beer/urls.py
@@ -5,20 +5,20 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/(?P<style_id>[0-9]+)/detail$',
-        views.beer_detail, name='beer_detail'),
-    url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/detail$',
-        views.beer_detail, name='beer_detail'),
-    url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/similar$',
-        views.similar_beers, name='similar_beers'),
-    url(r'^search/styles/$', views.styles, name='styles'),
-    url(r'^styles/(?P<style_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/(?P<style_id>[0-9]+)/$',
-        views.style_detail, name='style_detail'),
-    url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/not_found',
-        views.beer_not_found, name='beer_not_found'),
-    url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/different_styles',
-        views.styles_in_beer, name='styles_in_beer')
+url(r'^$', views.index, name='index'),
+url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/(?P<style_id>[0-9]+)/detail$',
+    views.beer_detail, name='beer_detail'),
+url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/detail$',
+    views.beer_detail, name='beer_detail'),
+url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/similar$',
+    views.similar_beers, name='similar_beers'),
+url(r'^search/styles/$', views.styles, name='styles'),
+url(r'^styles/(?P<style_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/(?P<style_id>[0-9]+)/$',
+    views.style_detail, name='style_detail'),
+url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=\'_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/not_found',
+    views.beer_not_found, name='beer_not_found'),
+url(r'^(?P<beer_name>[\w0-9 \-/\(\)""&,#!@\$%\^\*\{\}\[\]:;\.,\?~\+=_|\\<>’–¿·ªº¡°™“”‘—\u200e\u202c]+)/different_styles',
+    views.styles_in_beer, name='styles_in_beer')
 ]
 
-handler404 = 'handler404'
+handler404 = 'beer.views.handler404'
